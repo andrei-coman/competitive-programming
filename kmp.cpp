bff1ab2ce90cfdb4319db751c9b9ec1b472501e9
andrei-coman <59019422+andrei-coman@users.noreply.github.com>
2020-08-12 15:13:06 +0300

    Create kmp.cpp

---
+++ kmp.cpp
@@ -0,0 +1,25 @@
+int n, m;
+int N[1 + MAXN], M[1 + MAXN];
+
+int pi[1 + MAXN];
+inline int getPi(){
+    pi[1] = 0;
+    int k = 0;
+    for(int i = 2; i <= n; i++){
+        while(k > 0 && N[k + 1] != N[i])
+            k = pi[k];
+        if(N[k + 1] == N[i]) k++;
+        pi[i] = k;
+    }
+}
+
+int st[1 + MAXN], ind;
+inline void KMP(){
+    int k = 0;
+    for(int i = 1; i <= m; i++){
+        while(k > 0 && N[k + 1] != M[i])
+            k = pi[k];
+        if(N[k + 1]==M[i]) k++;
+        if(k == n) st[ind++] = i;
+    }
+}
