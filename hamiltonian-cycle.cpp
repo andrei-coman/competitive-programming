f5316dd05209272004d95c53c31a80a36ec0bf7e
andrei-coman <59019422+andrei-coman@users.noreply.github.com>
2020-08-12 15:10:48 +0300

    Update hamiltonian-cycle.cpp

--- hamiltonian-cycle.cpp
+++ hamiltonian-cycle.cpp
@@ -4,14 +4,14 @@ int C[1 << MAXN][MAXN];
 std::vector <int> G[MAXN];
 
 inline void hamiltonian(){
-    for(int i = 0; i < (1 << n); i++)
+	for(int i = 0; i < (1 << n); i++)
 		for(int j = 0; j < n; j++) C[i][j] = INF;
 	C[1][0] = 0;
 
 	for(int mask = 0; mask < (1 << n); mask++)
-	    for(int i = 0; i < n; i++) if(mask & (1 << i))
-        	for(auto y: G[i]) if(mask & (1 << y))
-            	C[mask][i] = std::min(C[mask][i], C[mask ^ (1 << i)][y] + Cost[y][i]);
+		for(int i = 0; i < n; i++) if(mask & (1 << i))
+			for(auto y: G[i]) if(mask & (1 << y))
+				C[mask][i] = std::min(C[mask][i], C[mask ^ (1 << i)][y] + Cost[y][i]);
 
 	ans = INF;
 	for(auto y: G[0])
